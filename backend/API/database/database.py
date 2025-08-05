c2a412429050182e10e85e2b865efd297786bca0
Emmankoko <emmankoko519@gmail.com>
2025-08-05 17:20:39 +0000

    Fix CI
    we are using a false link to the database for the sake of testing our build

--- backend/API/database/database.py
+++ backend/API/database/database.py
@@ -1,11 +1,11 @@
 # database.py
+import os
 from fastapi import FastAPI, Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
-#idk but my database name is Jesus, so if you want to test locally,
-# create your database server with Jesus as dbname or feel free to change on test branch
-DATABASE_URL = " however we decide to do. if we deiced to work on one databse, i share the URL of my server to you"
+#DATABASE_URL = " link to db here "
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
