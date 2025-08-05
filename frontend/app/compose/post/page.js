ad1fcda1a1ef566e2fdf37c9ee846ed5298eb660
RoyJake <royjake629@gmail.com>
2025-08-05 22:46:51 +0000

    progress on making the home page responsive

--- frontend/app/compose/post/page.js
+++ frontend/app/compose/post/page.js
@@ -14,9 +14,8 @@ export default function Post() {
       <svg viewBox="0 0 24 24" className=""><g><path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path></g></svg>
      </button></Link>
     <button id="post-button">Post</button>
-
    </nav>
-   <div>
+   <div id="textarea-container">
     <Image
      className="textarea-profile-image"
      src={'/emma.jpeg'}
