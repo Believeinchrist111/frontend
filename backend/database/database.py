6b4551b2f9de86cabd24533c021950129706336d
Emmankoko <emmankoko519@gmail.com>
2025-08-11 14:28:33 +0000

    use false database on github to avoid breaking the build
    we want to keep our databses private and then use sqllite only resolvable by
    github action's env variable

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/believe"
+DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
