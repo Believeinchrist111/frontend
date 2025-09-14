5c2bf2898a4b68bcbd479c102812d9ce48f6a222
RoyJake <royjake629@gmail.com>
2025-09-14 00:27:29 +0000

    progress

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -11,15 +11,18 @@ import SideNav from "../ui/side-nav.js";
 import "./profile.css"
 
 import { fetchUser } from "../lib/slices/userSlice";
+import { fetchPosts } from "../lib/slices/postSlice";
 
 
 
 export default function Profile() {
   const dispatch = useDispatch()
   const { user, loading, error } = useSelector((state) => state.user);
+  const { posts } = useSelector((state) => state.posts)
 
   useEffect(() => {
     dispatch(fetchUser())
+    dispatch(fetchPosts())
   }, [dispatch]);
 
   if (!user) return <p>Loading...</p>;
@@ -30,10 +33,11 @@ export default function Profile() {
         <div id="profile-wrapper">
           <ProfileNav user={user} />
           <ProfileInfoSection user={user} />
-          <TweetCard user={user}/>
-          <TweetCard user={user}/>
-          <TweetCard user={user}/>
-          <TweetCard user={user}/>
+          {posts.map((post) => (
+            <div key={post.id} className="border-b p-4">
+              <TweetCard user={user} post={post} />
+            </div>
+          ))}
         </div>
         <SideNav user={user} />
         <FooterNav />
